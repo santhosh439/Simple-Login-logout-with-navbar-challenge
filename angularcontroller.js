a034705635de13d78378db2df8b31fbcf97fef45
santhosh439 <45024421+santhosh439@users.noreply.github.com>
2018-11-13 21:38:01 -0600

    Add files via upload

---
+++ angularcontroller.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.firstname = "Santhosh Kumar";
+ $scope.angulardatabinding="This data is binded to angular controller";
+});
